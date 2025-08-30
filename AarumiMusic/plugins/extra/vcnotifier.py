f9a02ec050ae492dff0be51eb5a81c79a365d498
˹ ᴧᴧʀᴜᴍɪ ʙᴏᴛ's ˼ <214774540+itsAarumi@users.noreply.github.com>
2025-08-30 23:54:30 +0530

    Update vcnotifier.py

--- AarumiMusic/plugins/extra/vcnotifier.py
+++ AarumiMusic/plugins/extra/vcnotifier.py
@@ -9,12 +9,12 @@ logging.basicConfig(level=logging.INFO)
 async def video_chat_started(client, message: Message):
     chat = message.chat
     await message.reply(
-        f"<b>🎥 Vᴏɪᴄᴇ Cʜᴧᴛ ʜᴧs Sᴛᴧʀᴛᴇᴅ ɪɴ {chat.title}!</b>\n\n<b>ᴊᴏɪɴ ᴜs ɴᴏᴡ ғᴏʀ ᴧ ғᴜɴ ᴛɪᴍᴇ ᴛᴏɢᴇᴛʜᴇʀ..! 😉</b>"
+        f"<b>🎥 Vᴏɪᴄᴇ Cʜᴧᴛ ʜᴧs Sᴛᴧʀᴛᴇᴅ ɪɴ {chat.title}!</b>\n\n<b>ᴊᴏɪɴ ᴜs ɴᴏᴡ ꜰᴏʀ ᴧ ꜰᴜɴ ᴛɪᴍᴇ ᴛᴏɢᴇᴛʜᴇʀ..! 😉</b>"
     )
 
 @app.on_message(filters.video_chat_ended)
 async def video_chat_ended(client, message: Message):
     chat = message.chat
     await message.reply(
-        f"<b>🚫 Vᴏɪᴄᴇ Cʜᴧᴛ ʜᴧs Eɴᴅᴇᴅ ɪɴ {chat.title}.</b>\n\n<b>ᴛʜᴧɴᴋ ʏᴏᴜ ғᴏʀ ᴊᴏɪɴɪɴɢ..! sᴇᴇ ʏᴏᴜ ɴᴇxᴛ ᴛɪᴍᴇ..! 👋</b>"
+        f"<b>🚫 Vᴏɪᴄᴇ Cʜᴧᴛ ʜᴧs Eɴᴅᴇᴅ ɪɴ {chat.title}.</b>\n\n<b>ᴛʜᴧɴᴋs ʏᴏᴜ ꜰᴏʀ ᴊᴏɪɴɪɴɢ..! sᴇᴇ ʏᴏᴜ ɴᴇxᴛ ᴛɪᴍᴇ..! 👋</b>"
     )
